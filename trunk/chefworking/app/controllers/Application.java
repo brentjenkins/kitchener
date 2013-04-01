74ea474fdcb2debc8208bfb4785ffbbd78c7bb7f
Brent Jenkins <brentjenkins@gmail.com>
2013-04-01 09:01:59 -0700

    added placeholder routes

--- trunk/chefworking/app/controllers/Application.java
+++ trunk/chefworking/app/controllers/Application.java
@@ -1,14 +1,57 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
 
-import views.html.*;
+import scala.Long;
 
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok("Hello world!");
     }
-  
+
+    public static Result profile() {
+        return TODO;
+    }
+
+    public static Result editProfile() {
+        return TODO;
+    }
+
+    public static Result jobHistories() {
+        return TODO;
+    }
+
+    public static Result newJobHistory() {
+        return TODO;
+    }
+
+    public static Result deleteJobHistory(Long id) {
+        return TODO;
+    }
+
+    public static Result jobPostings() {
+        return TODO;
+    }
+
+    public static Result newJobPosting() {
+        return TODO;
+    }
+
+    public static Result deleteJobPosting(Long id) {
+        return TODO;
+    }
+
+    public static Result restaurants() {
+        return TODO;
+    }
+
+    public static Result askForReferral() {
+        return TODO;
+    }
+
+    public static Result provideReferral() {
+        return TODO;
+    }
+
 }
